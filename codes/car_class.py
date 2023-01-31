55ad5d831532fa0a3ce28c7e18ad39513eb8a0fb
Michael Paterson <michaelpaterson194@gmail.com>
2023-01-31 10:25:14 +0000

    _

---
+++ codes/car_class.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, current_speed, max_speed, fuel_level):
+        self.current_speed = current_speed
+        self.max_speed = max_speed
+        self.fuel_level = 0
+    
+    def accelerate(self):
+        self.current_speed += 1
+        self.fuel_level -= 1
+        
+    def brake(self):
+        self.current_speed -= 1
+        
+    def refuel(self):
+        self.fuel_level += 1
+        
